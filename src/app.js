badeec2d2f277bf1cc946e9b13a16c67f077a8d9
ricar <ricar@RICARDOKIRITO>
2024-08-06 10:00:48 -0400

    nodemon

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('dev'));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://nicochat.netlify.app/",
     credentials: true,
 }))
 app.use("/api",authRoutes)
